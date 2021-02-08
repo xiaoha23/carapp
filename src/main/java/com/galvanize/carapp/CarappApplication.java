ef7a86ac495b04ab3df282ea998060a1d93f9917
srijan4friends <srijan4friends@gmail.com>
2021-02-08 17:47:23 -0600

    Initial commit

---
+++ src/main/java/com/galvanize/carapp/CarappApplication.java
@@ -0,0 +1,13 @@
+package com.galvanize.carapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/galvanize/carapp/CarappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galvanize.carapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
